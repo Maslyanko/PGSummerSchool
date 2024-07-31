cbe3d6e5dbe3425e353234301d435d2b608bc70b
Maslyanko <a.maslyanko@g.nsu.ru>
2024-07-31 16:51:59 +0700

    Я ошибся, извините

--- utils/config/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-//Функция, которая парсит файл proxy.conf и регистрирует все конфигурационные переменные в SVC.
-void parseConfig(void);
--- utils/utils/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-#define FILE_TYPE 1
-#define DIRECTORY_TYPE 2
-
-//Просто штука, которая позволяет из нескольких наименований директорий/файлов для создания полного пути до файла/директории.
-//number - кол-во строк.
-//type - тип, который может быть либо файлом (FILE_TYPE), либо директорией (DIRECTORY_TYPE).
-char* makeFullName(int number, int type, ...);
